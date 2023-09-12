2d52c019f81108e3e1887123bccb1189558f26a1
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2023-09-12 17:18:36 +0100

    multiples

--- src/Assignment/Multiples.java
+++ src/Assignment/Multiples.java
@@ -1,2 +1,10 @@
-package Assignment;public class Multiples {
+package Assignment;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int number = 7;
+        int numberOne = 9;
+        int product = number * numberOne;
+        System.out.println(product);
+    }
 }
