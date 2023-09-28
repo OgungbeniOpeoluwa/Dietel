374accb3b3544b47c145d7c056c27952cbae6a27
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2023-09-28 11:27:34 +0100

    update

--- src/chibuzorAssignment/UberScore.java
+++ src/chibuzorAssignment/UberScore.java
@@ -4,17 +4,24 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class UberScore {
-//    public static void main(String[] args) {
-//        Scanner scanner =  new Scanner(System.in);
-//         int scores =[];
-//        for(int number = 1;number<=10;number++){
-//            System.out.println("Enter your score :");
-//            int score = scanner.nextInt();
-//            //System.out.println(Arrays.toString(scores));
-//
-//        }
-//
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] scores = new int[10];
+        int largest = 0;
+        for (int number = 0; number < 10; number++) {
+            System.out.println("Enter your score :");
+            int score = scanner.nextInt();
+            scores[number] = score;
+            if (scores[number] > largest){
+                largest = score;
+            }
+
+        }
+        System.out.println(Arrays.toString(scores));
+        System.out.println("The largest number is "+largest);
+
 
     }
+}
 
 
--- src/practise/SquareRoot.java
+++ src/practise/SquareRoot.java
@@ -1,13 +1,16 @@
 package practise;
 
 public class SquareRoot {
+    public static int squareRoot(int userInput) {
+        int answer = 0;
+        for (int number = 1; number <= userInput; number++) {
+           if (number * number == userInput) {
+               answer = number;
+           }
+        }
+        return answer;
+    }
 
-//    public static int squareRoot(int userInput) {
-//        for (int number = 1; number <= userInput; number++) {
-//            if (number * number == userInput) {
-//                return number;
-//            }
-//        }
-//    }
-}
-//
+
+   }
+
