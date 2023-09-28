b28420d5a531f43a91f22eeb5b9ce3db3231c826
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2023-09-28 22:46:28 +0100

    update

---
+++ src/chibuzorAssignment/ScoresArray.java
@@ -0,0 +1,42 @@
+package chibuzorAssignment;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ScoresArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int [] scores = new int[10];
+        System.out.println("Enter your score : ");
+        int score = scanner.nextInt();
+        scores[0] = score;
+        int result = scores[0];
+        int minimum = scores[0];
+        int maximum =scores[0];
+        String  odd = "";
+        for(int count = 1;count < scores.length;count++){
+            System.out.println("Enter your score : ");
+            score = scanner.nextInt();
+            scores[count] = score;
+            result += scores[count];
+            if(scores[count] < minimum){
+                minimum = scores[count];
+            }
+            if(scores[count] > maximum){
+                maximum = scores[count];
+            }
+            if(scores[count] % 2 ==1){
+               odd += scores[count] + "\t";
+            }
+
+        }
+
+        int average = result / scores.length;
+        System.out.println(Arrays.toString(scores));
+        System.out.println(average);
+        System.out.println("the total is :" +result);
+        System.out.println("the maximum is :"+maximum);
+        System.out.println("the minimum is :"+minimum);
+        System.out.println("the odd numbers are : " +odd);
+    }
+}
