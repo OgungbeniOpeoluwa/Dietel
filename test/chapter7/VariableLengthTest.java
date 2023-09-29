b83f46b02d840dd72d9e5259e31bbe42be2bcc55
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2023-09-30 00:06:31 +0100

    update

--- test/chapter7/VariableLengthTest.java
+++ test/chapter7/VariableLengthTest.java
@@ -1,4 +1,26 @@
+package chapter7;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class VariableLengthTest {
-  
+    @Test
+    public void testFirstCall(){
+        double result = VariableLength.printAverage(1,2,3,4,5);
+        assertEquals(3,result);
+    }
+    @Test
+    public void testSecondCall(){
+        double result = VariableLength.printAverage(1,2,3,4);
+        assertEquals(2,result);
+    }
+    @Test
+    public void testThirdCall() {
+        double result = VariableLength.printAverage(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        assertEquals(5.0, result);
+    }
+
+
+
 }
