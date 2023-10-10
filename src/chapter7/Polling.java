0a3f928ea9c27d2b29a4c3e3d74ec28ffad88f30
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2023-10-10 05:08:07 +0100

    update

--- src/chapter7/Polling.java
+++ src/chapter7/Polling.java
@@ -1,103 +1,97 @@
 package chapter7;
 
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Polling {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello,This a Social issue polling unit,kindly press (1) to continue or press (0) to log out");
-        int stoper = scanner.nextInt();
-        ArrayList<Integer> lqbt = new ArrayList<>();
-        ArrayList<Integer> mentalHealth = new ArrayList<>();
-        ArrayList<Integer>  healthCareAccess = new ArrayList<>();
-        ArrayList<Integer> dataSecurity = new ArrayList<>();
-        ArrayList<Integer> gunControl = new ArrayList<>();
-        int total = 0;
-        int result = 0;
+        int [] result = new int[5];
+        int lgbtq = 0;
+        int  mentalHealth = 0;
+        int healthCareAccess = 0;
+        int dataInsecurity = 0;
+        int gunControl = 0;
         int counts = 0;
-        int numb = 0;
+        String[] socialIssues = {"LGBTQ RIGHT", "MENTAL HEALTH", "HEALTHCARE ACCESS", "DATA INSECURITY", "GUN CONTROL"};
+        int[][] response = new int[5][10];
+        System.out.println("Kindly enter your rate on this issues where 1 is the least and 10 is the most important");
+        int stoper = 1;
+        while (stoper != 0) {
+            for (int number = 0; number < socialIssues.length; number++) {
+                System.out.println(socialIssues[number]);
+                String input = scanner.nextLine();
+                int userInput = Integer.parseInt(input);
+                while(userInput < 1 || userInput > 10){
+                    System.out.println("Kindly enter a valid number between 1 - 10,where 1 is the least and 10 is the most important: ");
+                    input = scanner.nextLine();
+                    userInput = Integer.parseInt(input);
+                }
+                switch(number){
+                    case 0 -> {
+                        lgbtq += userInput;
+                        result[number] = lgbtq;
+                    }
+                    case 1 ->{
+                            mentalHealth += userInput;
+                        result[number] = mentalHealth;
+                    }
+                    case 2 -> {
+                        healthCareAccess += userInput;
+                        result[number] = healthCareAccess;
+                    }
+                    case 3 -> {
+                        dataInsecurity += userInput;
+                        result[number] = dataInsecurity;
+                    }
+                    case 4 -> {
+                        gunControl += userInput;
+                        result[number] = gunControl;
+
+                    }
+                }
+                if (userInput == 1) {
+                    counts = 0;
+                } else {
+                    counts = userInput - 1;
+                }
+                response[number][counts] += 1;
+            }
+            System.out.println("Will you like  ");
+            stoper = scanner.nextInt();
+        }
+        int inputss;
         int results = 0;
-        int count = 0;
-        String[] socialIssues = {"LQBTQ RIGHT", "MENTAL HEALTH", "HEALTHCARE ACCESS", "DATA INSECURITY", "GUN CONTROL"};
-      int[] response = new int[10];
-      while (stoper != 0) {
-          System.out.println("""
-                  on the rate of 1 - 10,where 1 is the least important and 10 is the most important,
-                             Kindly rate the issue""");
-          for (int number = 0; number < socialIssues.length; number++) {
-              for (int numbss = 1; numbss < response.length; numbss++) {
-                  response[numbss] = numbss;
-              }
-              System.out.println(socialIssues[number] + " " + Arrays.toString(response));
-              int input = scanner.nextInt();
-              switch (number) {
-                  case 0 -> {
-                      lqbt.add(input);
-                      total += input;
-                  }
-                  case 1 -> {
-                      mentalHealth.add(input);
-                      result += input;
-                  }
-                  case 2 -> {
-                      healthCareAccess.add(input);
-                      counts += input;
-                  }
-                  case 3 -> {
-                      dataSecurity.add(input);
-                      numb += input;
-                  }
-                  case 4 -> {
-                      gunControl.add(input);
-                      results += input;
-                  }
-              }
+        String max = " ";
+        String min = " ";
+        int maximum = result[0];
+        int mininimum = result[0];
+        System.out.println("SOCIAL ISSUES " + " 1" + " 2 "
+                          + " 3 " + " 4 " + " 5 " + " 6 " + " 7 " + " 8 " + " 9 " + " 10 ");
+        for (int counter = 0; counter < socialIssues.length; counter++) {
+            System.out.print(socialIssues[counter] + "   ");
+            for (int numb = 0; numb < 10; numb++) {
+                inputss = response[counter][numb];
+                if(inputss > 0) {
+                    results +=inputss;
+                }
+              System.out.print(inputss +  "   " + "  " + " ");
+            }
+            System.out.print(result[counter]/results);
+            if(result[counter] > maximum){
+                maximum = result[counter];
+                max = socialIssues[counter];
+            }
+            if(result[counter] < mininimum){
+                mininimum = result[counter];
+                min = socialIssues[counter];
+            }
+            results -= results;
+            System.out.println(" ");
+             }
+        System.out.println("The highest social issue is  " + max + " with the total points of: " + maximum);
+        System.out.println("The lowest social issue  is  " + min + "  with the total points of : " + mininimum);
 
-          }
-          count++;
-          System.out.println("Hello,This a Social issue polling unit,kindly press (1) to continue or press (0) to log out");
-          stoper = scanner.nextInt();
-      }
-      total/=count;
-      result/=count;
-      count/= count;
-      numb/= count;
-      results/=numb;
 
-        System.out.print("LQBT:  " );
-                 for(int numbs : lqbt){
-                     System.out.print(numbs + "  " + " " + " ");
-                 }
-        System.out.print(total);
-        System.out.println();
-        System.out.print("MENTAL HEALTH CARE :  ");
-                 for(int numbs : mentalHealth){
-                    System.out.print(numbs + "  " + " "+ " ");
-                }
-        System.out.print(result);
-        System.out.println();
-        System.out.print("HEALTH CARE ACCESS:  ");
-                for(int numbs : healthCareAccess){
-                    System.out.print(numbs + "  " + " ");
-                }
-        System.out.print(count);
-        System.out.println();
-        System.out.print("DATA SECURITY:  ");
-                for(int numbs : dataSecurity){
-                    System.out.print(numbs + "  " + " ");
-                }
-        System.out.print(numb);
-        System.out.println();
-        System.out.print("GUN CONTROL:  ");
-                for(int numbs : gunControl){
-                    System.out.print(numbs + "  " + " ");
-                }
-        System.out.print(results);
-
+        }
 
     }
-
-}
