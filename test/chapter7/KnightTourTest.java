6bf1c82087791b2945c0bc86f572f9e672bbb13f
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2023-10-24 04:12:19 +0100

    first work on Knight Tour

---
+++ test/chapter7/KnightTourTest.java
@@ -0,0 +1,13 @@
+package chapter7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KnightTourTest {
+    @Test
+    public void testAllSquareInitialiseToZero(){
+
+    }
+
+}
