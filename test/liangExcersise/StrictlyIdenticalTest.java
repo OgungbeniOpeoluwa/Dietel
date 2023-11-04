462febe0223fcecce161f296227a31b84b9c2722
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2023-11-04 08:37:27 +0100

    STRICTLY IDENTICAL ARRAYLIST

--- test/liangExcersise/StrictlyIdenticalTest.java
+++ test/liangExcersise/StrictlyIdenticalTest.java
@@ -1,4 +1,27 @@
+package liangExcersise;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StrictlyIdenticalTest {
-  
+    @Test
+    public void testIfNumberIsStrictlyIdentical(){
+        int [] array = {1,2,3,4,5};
+        int [] arrays = {1,2,3,4,5};
+        assertTrue(StrictlyIdentical.isStrictlyIdentical(array,arrays));
+    }
+    @Test
+    public void testIfNumberIsNotStrictlyIdentical(){
+        int [] array = {2,1,3,4,5,8};
+        int [] arrays = {1,2,3,4,5,6};
+        assertFalse(StrictlyIdentical.isStrictlyIdentical(array,arrays));
+    }
+    @Test
+    public void testIfNumberIsNotTheSameLength(){
+        int [] array = {1,2,3,4,5};
+        int [] arrays = {1,2,3,4,5,6};
+        assertFalse(StrictlyIdentical.isStrictlyIdentical(array,arrays));
+    }
+
 }
