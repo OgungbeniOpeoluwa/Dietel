20334611dc1cf566c998ac23a0126dc1af78204b
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2023-11-12 23:39:16 +0100

    11/12/2023

--- src/liangExcersise/partitionalist.java
+++ src/liangExcersise/partitionalist.java
@@ -1,5 +1,31 @@
 package liangExcersise;
 
+import java.util.ArrayList;
+
 public class partitionalist {
 
+
+    public static int[] pivot(int[] numb) {
+        ArrayList <Integer> smallest = new ArrayList<>();
+        ArrayList <Integer> largest = new ArrayList<>();
+        int result = numb[0];
+        for(int count = 1; count < numb.length;count++){
+            if(numb[count] > result){
+                largest.add(numb[count]);
+            }
+            else{
+                smallest.add(numb[count]);
+            }
+        }
+        smallest.add(result);
+        smallest.addAll(largest);
+        return changeArraylistToArray(smallest);
+    }
+    private static int[] changeArraylistToArray(ArrayList <Integer> numbers){
+        int [] numb = new int [numbers.size()];
+        for(int count = 0; count < numbers.size();count++){
+            numb[count] = numbers.get(count);
+        }
+        return numb;
+    }
 }
--- test/liangExcersise/partitionalistTest.java
+++ test/liangExcersise/partitionalistTest.java
@@ -7,6 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class partitionalistTest {
     @Test
     public void testForPartionOfAList(){
+        int [] numb = {5, 2, 9, 3, 6, 8};
+        int [] expected = {2,3,5,9,6,8};
+        assertArrayEquals(expected,partitionalist.pivot(numb));
+
+    }
+    @Test
+    public void testForPartionList(){
+        int [] numb = {10 ,1 ,5, 16, 61, 9, 11 ,1};
+        int [] expected = { 1, 5 ,9, 1, 10 ,16 ,61, 11};
+        assertArrayEquals(expected,partitionalist.pivot(numb));
 
     }
 
