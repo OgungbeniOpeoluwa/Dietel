c38490b224f4d15dcf7d39e329c8d1ac93f02d61
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2023-11-15 21:36:47 +0100

    Integer To Roman Figure

--- src/practise/RomanFigure.java
+++ src/practise/RomanFigure.java
@@ -1,12 +1,121 @@
 package practise;
 
+import java.util.ArrayList;
+
 public class RomanFigure {
-    static int [] arraysOfNumbers = {1,5,10,50,100,500,1000};
-    static char [] romanFigures = {'i','v','x','l','c','d','m'};
+    static int[] arraysOfNumbers = {1, 5, 10, 50, 100, 500, 1000};
+    static char[] romanFigures = {'i', 'v', 'x', 'l', 'c', 'd', 'm'};
+    static ArrayList <String> results = new ArrayList<>();
 
-    public static String numberToRomanFigure(int input) {
-        String answer = "";
-        return answer;
+    public static String numberToRomanFigure(int num){
+        String input = String.valueOf(num);
+        int count = input.length();
+        ArrayList <String> result = new ArrayList<>();
+        if(count > 0){
+            for(int numb = 0; numb < input.length(); numb++){
+                count --;
+                result.add(input.charAt(numb) + addZeros(count) );
+            }
+        }
+        for(int counts = 0; counts < result.size(); counts++){
+            int numbers = Integer.parseInt(result.get(counts));
+            if(numbers == 0){
+                continue;
+            }
+            int numb = binaryCheck(numbers);
+            if(numb > 0 ){
+                results.add(String.valueOf(romanFigures[numb]));
+            }
+            else{
+                int check = rangeCheck(numbers);
+                int returns = 0;
+                if(numbers > arraysOfNumbers[check]) {
+                    returns = numbers - arraysOfNumbers[check] ;
+                }
+                else {
+                   returns = arraysOfNumbers[check] - numbers;
+               }
+                if(binaryCheck(returns) >= 0 ) {
+                    String reset = romanFigures[binaryCheck(returns)] + "" + romanFigures[check];
+                    results.add(reset);
+                }
+                else{
+                    checker(numbers);
 
+                }
+
+            }
+        }
+        return finalResult();
     }
+     private static String addZeros(int count){
+        String result = "";
+        for(int counts = 0; counts < count ; counts++){
+            result += "0";
+        }
+        return result;
+     }
+     private static  int rangeCheck(int number){
+        int max = 0;
+        if(number > arraysOfNumbers[arraysOfNumbers.length-1]){
+            max = arraysOfNumbers.length-1;
+        }
+        else {
+            for(int count = 0; count < arraysOfNumbers.length; count++){
+                if(arraysOfNumbers[count] > number){
+                    max = count;
+                    break;
+                }
+            }
+        }
+        return  max;
+     }
+     private static int binaryCheck(int number){
+        int returns = -1;
+        for(int count = 0; count < arraysOfNumbers.length; count++){
+            if(number == arraysOfNumbers[count]){
+                returns = count;
+                break;
+            }
+        }
+        return returns;
+     }
+
+     private static void checker(int number){
+        int check = 0;
+        if(number > arraysOfNumbers[arraysOfNumbers.length-1]){
+            check = rangeCheck(number);
+        }
+       else{
+           check = rangeCheck(number) - 1;
+        }
+         while (number != 0){
+            int result = number - arraysOfNumbers[check];
+            if (result >= 0) {
+                results.add(String.valueOf(romanFigures[check]));
+                number = result;
+            } else {
+                check--;
+            }
+        }
+     }
+     private static String finalResult(){
+        String result = "";
+        for(int count = 0; count < results.size();count++){
+            result+=results.get(count);
+        }
+        results.clear();
+        return result;
+     }
+
+
 }
+
+
+
+
+
+
+
+
+
--- test/practise/RomanFigureTest.java
+++ test/practise/RomanFigureTest.java
@@ -6,13 +6,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RomanFigureTest {
     @Test
-    public void testForRomanFigure(){
-       // int number = 30;
-       // String result = "xxx";
-        System.out.println(RomanFigure.numberToRomanFigure(90));
-       // assertEquals(result,RomanFigure.numberToRomanFigure(number));
-
-
+    public void testForRomanFigure() {
+        int number = 30;
+        String result = "xxx";
+        assertEquals(result, RomanFigure.numberToRomanFigure(number));
     }
+    @Test
+    public void testForRomanFigureOf99() {
+        int number = 99;
+        String result = "xcix";
+        assertEquals(result, RomanFigure.numberToRomanFigure(number));
+    }
+    @Test
+    public void testForRomanFigureOf45() {
+        int number = 45;
+        String result = "xlv";
+        assertEquals(result, RomanFigure.numberToRomanFigure(number));
+    }
+
+    @Test
+    public void testForRomanFigureOf80() {
+        int number = 80;
+        String result = "lxxx";
+        assertEquals(result, RomanFigure.numberToRomanFigure(number));
+    }
+
+    @Test
+    public void testForRomanFigureOf165() {
+        int number = 165;
+        String result = "clxv";
+        assertEquals(result, RomanFigure.numberToRomanFigure(number));
+    }
+
+    @Test
+    public void testForRomanFigureOf104() {
+        int number = 1994;
+        String result = "mcmxciv";
+        assertEquals(result, RomanFigure.numberToRomanFigure(number));
+    }
+    @Test
+    public void testForRomanFigureOf2000() {
+        int number = 3000;
+        String result = "mmm";
+        assertEquals(result, RomanFigure.numberToRomanFigure(number));
+    }
+
+
+
+
 
 }
